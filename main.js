8a37872190a7e2740994a2acb37d8d258260c068
Jordan <jordanfhoughton@gmail.com>
2024-08-09 16:29:49 +0100

    woop[

--- main.js
+++ main.js
@@ -3,9 +3,12 @@ import { caesarCipher, caesarDecipher} from "./solution.js";
 const today = new Date();
 const year = today.getFullYear();
 const day = today.getDate();
-
 let code = year - 2000 - day;
 
-console.log(caesarCipher('The troops are inbound', code));
 
-caesarCipher('The troops are inbound', code);
+const message = caesarCipher('Hello world', code);
+
+const decipheredMessage = caesarDecipher(message, code);
+
+console.log(decipheredMessage);
+
