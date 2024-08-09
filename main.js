09ee4f938e2823a03fa20b27f6bfba0ee3c14e07
Jordan <jordanfhoughton@gmail.com>
2024-08-09 16:25:59 +0100

    main

---
+++ main.js
@@ -0,0 +1,12 @@
+import { c } from "@vitest/runner/dist/tasks-zB5uPauP.js";
+import { caesarCipher, caesarDecipher} from "./solution.js";
+
+const today = new Date();
+const year = today.getFullYear();
+const day = today.getDate();
+
+let code = year - 2000 - day;
+
+console.log(caesarCipher('The troops are inbound', code));
+
+caesarCipher('The troops are inbound', code);
